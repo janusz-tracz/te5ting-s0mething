38dcfea4648ae8bb3c7c5de426e7f4a2367b745f
Kamilbur <kamil.burkiewicz@gmail.com>
2021-05-13 18:12:03 +0200

    laasldjf

--- src/test/java/com/gupb/manager/python/PythonRunnerTest.java
+++ src/test/java/com/gupb/manager/python/PythonRunnerTest.java
@@ -1,13 +1,42 @@
 package com.gupb.manager.python;
 
 import org.junit.jupiter.api.Test;
+import com.gupb.manager.model.BotStatus;
+import com.gupb.manager.model.Team;
+import com.gupb.manager.python.PythonRunner;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+
+@RunWith(SpringRunner.class )
+@SpringBootTest
 public class PythonRunnerTest {
+    @Autowired
     private static final PythonRunner p = new PythonRunner();
 
     @Test
     public void runTest() {
         //
+/*
+        for (int ii = 0; ii < 5; ii++) {
+            System.out.println("Hello, world!");
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+*/
+        PythonPackageManager pkg = new PythonPackageManager();
+
+        pkg.createVirtualEnvironment("/home/burooo/IO-TEST/testXD/te5ting-s0mething/src/main/java/com/gupb/manager/python/GUPB-test/", "nejm");
+
+        /*
         Thread t = new Thread(p);
         t.start();
         try {
@@ -15,5 +44,6 @@ public class PythonRunnerTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        */
     }
 }
